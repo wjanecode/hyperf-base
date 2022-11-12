3b769f69f8a2c54fcdc28c381991ba3c65fd2ff4
wujian <mac@mac.com>
2022-11-12 15:16:21 +0800

    feat facade auth

---
+++ src/Facade/Auth.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+
+namespace WJaneCode\HyperfBase\Facade;
+
+use Hyperf\Utils\ApplicationContext;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Qbhy\HyperfAuth\Authenticatable;
+use Qbhy\HyperfAuth\AuthManager;
+
+/**
+ * token 鉴权
+ * 对
+ */
+class Auth
+{
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function authManager()
+    {
+        return ApplicationContext::getContainer()->get(AuthManager::class);
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function user(): ?Authenticatable
+    {
+        return self::authManager()->user();
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function userId()
+    {
+        return self::user()->getId();
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function isLogin(): bool
+    {
+        return self::authManager()->check();
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function isGuest(): bool
+    {
+        return self::authManager()->guest();
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function login(Authenticatable $user)
+    {
+        return self::authManager()->login($user);
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function logout()
+    {
+        return self::authManager()->logout();
+    }
+}
